6ddc5d70f830e7eddd35751dc5ccc2a2a0d33a27
Tnixc <tnixxc@gmail.com>
2024-04-09 22:00:51 -0400

    implemented more functions

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,16 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+
     if args.len() < 2 {
         println!("Please provide a url!");
         return;
+    } else if !&args[1].starts_with("https://") {
+        println!("Please provide a valid url! (including https://)");
+        return;
     }
-    let word = &args[1];
-    println!("{}", utils::download_html(word))
+    let url = &args[1];
+    let body = utils::download_html(&url);
+    println!("{:?}", utils::get_title(&body));
+    println!("{:?}", utils::get_read_now_link(&body));
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,43 @@
-pub fn download_html(url: &String) -> &String {
-    return url
+use core::panic;
+
+extern crate reqwest;
+
+pub fn download_html(url: &String) -> String {
+    let req = reqwest::blocking::get(url);
+    let res = match req {
+        Ok(body) => body,
+        Err(er) => panic!("Problem with downloading html: {:?}", er)
+    };
+    let body = res.text();
+    return match body {
+        Ok(z) => z,
+        Err(er) => panic!("Problem with converting body to text {:?}", er)
+    }
+}
+
+pub fn get_title(html: &String) -> &str {
+    let start = "<title>";
+    let end = "</title>";
+    let start_position = html.find(start);
+
+    if start_position.is_some() {
+        let start_position = start_position.unwrap() + start.len();
+        let html = &html[start_position..];
+        let end_position = html.find(end).unwrap_or_default();
+        let in_tag = &html[..end_position];
+        if in_tag.contains("|") {
+            let end = in_tag.find("|").unwrap_or_default();
+            return &in_tag[..end]
+        } else {
+            return in_tag;
+        }
+    } else {
+        panic!("something went wrong with parsing the html!")
+    }
+}
+
+pub fn get_read_now_link(html: &String) -> String {
+    let lines = html.split("\n");
+    let filtered_lines : String = lines.into_iter().filter(|&z| z.contains("readchapterbtn") || z.contains("Read Now")).collect();
+    return filtered_lines;
 }
