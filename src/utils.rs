1b1cbb231808853e32707c33687b22784c6353fb
Tnixc <tnixxc@gmail.com>
2024-04-10 12:50:45 -0400

    thing

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 use core::panic;
-
 extern crate reqwest;
+
 
 pub fn download_html(url: &String) -> String {
     let req = reqwest::blocking::get(url);
@@ -15,29 +15,25 @@ pub fn download_html(url: &String) -> String {
     }
 }
 
-pub fn get_title(html: &String) -> &str {
-    let start = "<title>";
-    let end = "</title>";
-    let start_position = html.find(start);
-
-    if start_position.is_some() {
-        let start_position = start_position.unwrap() + start.len();
-        let html = &html[start_position..];
-        let end_position = html.find(end).unwrap_or_default();
-        let in_tag = &html[..end_position];
-        if in_tag.contains("|") {
-            let end = in_tag.find("|").unwrap_or_default();
-            return &in_tag[..end]
-        } else {
-            return in_tag;
-        }
+pub fn get_title(html: &String) -> String {
+    let in_tag = get_substring_between(&html, "<title>", "</title>");   
+    if in_tag.contains("|") {
+        let end = in_tag.find("|").unwrap_or_default();
+        return in_tag[..end].to_string();
     } else {
-        panic!("something went wrong with parsing the html!")
+        return in_tag;
     }
 }
 
 pub fn get_read_now_link(html: &String) -> String {
     let lines = html.split("\n");
-    let filtered_lines : String = lines.into_iter().filter(|&z| z.contains("readchapterbtn") || z.contains("Read Now")).collect();
-    return filtered_lines;
+    return lines.into_iter().filter(|&z| z.contains("readchapterbtn") || z.contains("Read Now")).collect();
+    // return filtered_lines;
+}
+
+fn get_substring_between(str: &String, start: &str, end: &str) -> String {
+let start_bytes = str.find(start).unwrap_or(0); 
+let end_bytes = str.find(end).unwrap_or(str.len()); 
+let result = &str[start_bytes..end_bytes]; 
+    return result.to_string();
 }
