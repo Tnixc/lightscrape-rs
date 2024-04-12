08334b070e0634e411d4f695a815150ad8c09f8b
Tnixc <tnixxc@gmail.com>
2024-04-12 12:30:57 -0400

    implement good loading

--- src/sync_mode.rs
+++ src/sync_mode.rs
@@ -1,6 +1,8 @@
 use crate::utils::*;
-
 use futures::future::*;
+use indicatif::{HumanDuration, ProgressBar, ProgressStyle};
+use std::time::Duration;
+use std::time::Instant;
 
 pub fn get_read_now_link(html: &String, url: &String) -> String {
     let line: String = html
@@ -62,19 +64,40 @@ pub fn get_next_link(html: &String, url: &String) -> String {
 pub async fn sync_main(main_url: &String, main_body: &String) -> () {
     let chapter_1_url = get_read_now_link(&main_body, &main_url);
 
-    fn recurse(url: String, i: i32) -> BoxFuture<'static, i32> {
-        async move {
-            let body = &download_html(&url.to_string()).await;
-            let next = get_next_link(body, &url.to_string());
-            if next.is_empty() {
-                return i;
-            }
-            let path = "./res/".to_string() + "[" + i.to_string().as_str() + "] " + ".md";
-            let _ = tokio::fs::File::create(&path).await;
-            let _ = tokio::fs::write(&path, parse_content(body)).await;
-            return recurse(next, i + 1).await;
+    let started = Instant::now();
+    let pb = ProgressBar::new_spinner();
+    pb.enable_steady_tick(Duration::from_millis(80));
+    pb.set_style(
+        ProgressStyle::with_template("{spinner:.blue} {msg}")
+            .unwrap()
+            .tick_strings(&[
+                "[    ]", "[=   ]", "[==  ]", "[=== ]", "[====]", "[ ===]", "[  ==]", "[   =]",
+                "[    ]", "[   =]", "[  ==]", "[ ===]", "[====]", "[=== ]", "[==  ]", "[=   ]",
+                "[====]",
+            ]),
+    );
+
+    recurse(chapter_1_url, 1, pb).await;
+
+    let duration = started.elapsed();
+    let human_readable = HumanDuration(duration);
+    println!("Took {}", human_readable);
+}
+
+fn recurse(url: String, i: i32, pb: ProgressBar) -> BoxFuture<'static, i32> {
+    async move {
+        let body = &download_html(&url.to_string()).await;
+        let next = get_next_link(body, &url.to_string());
+        pb.set_message(format!("Downloading chapter {}", i));
+        if next.is_empty() {
+            pb.finish_with_message(format!("Finished downloading {} chapters", i));
+            return i;
         }
-        .boxed()
+        let path = "./res/".to_string() + "[" + i.to_string().as_str() + "] " + ".md";
+        let _ = tokio::fs::File::create(&path).await;
+        let _ = tokio::fs::write(&path, parse_content(body)).await;
+
+        return recurse(next, i + 1, pb).await;
     }
-    recurse(chapter_1_url, 1).await;
+    .boxed()
 }
